f6781097b4732a1460917b915c49cf5925e89f44
felecia <fl248@njit.edu>
2018-06-19 14:05:46 -0400

    Laravel setup

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,7 +4,10 @@
 
    <div class="container">
         <h1>About me</h1>
-        <p class="lead">Hi My Name is Felecia. I am the website owner. Please feel free to contact me at anytime.</p>
+        <p class="lead">Hi My Name is Felecia. Baking deserts is my passion. Well actually eating desserts
+            is my passion. I have a bachelor's degree in Culinary Science from the CIA in New York. I have
+            as a pastry chef for some of the best restaurants in New York. Now I am sharing my knowledge
+            to others who have always had a desire to be a professional baker. </p>
    </div>
 
 @endsection
