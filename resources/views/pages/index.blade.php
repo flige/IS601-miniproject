f4efabd8e95c846913a5a7a0c0b6c8705a2d3858
felecia <fl248@njit.edu>
2018-06-19 19:49:50 -0400

    Laravel setup

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,31 +12,24 @@
                 <div class="carousel-item active">
                     <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
                     <div class="container">
-                        <div class="carousel-caption text-left">
-                            <h1>Thank you for visting my webpage.</h1>
-                            <p> Come Again</p>
+                        <div class="carousel-caption text-center">
+                            <h1>Felecia's Pastry School</h1>
+                            <p> Fulfill your pastry dreams here!</p>
                             <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
                         </div>
                     </div>
                 </div>
 
             </div>
-            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-        <div class="row">
-            <div class="col-lg-4">
-                <img class="rounded-circle" src="https://assets.marthastewart.com/styles/wmax-300/d23/med104889_1009_hom001_l/med104889_1009_hom001_l_vert.jpg?itok=KurVjTut alt=">
-                <p>I love cookies</p>
-            </div><!-- /.col-lg-4 -->
 
-        </div><!-- /.row -->
+        </div>
+        <div class="container text-center">
+                <h2>Learn how to create edible works of art!!!</h2>
+                <img class="rounded-circle" src="http://finedininglovers.cdn.crosscast-system.com/BlogPost/xl_10064_patisserie-ciel-finedininglovers-TP.jpg">
+
+            </div>
+
+        </div>
     </div>
 
 @endsection
